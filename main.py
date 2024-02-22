3b7a6925bfaf3f4f37344d9037ead905b2d7ada3
Mikhaylov Nikita <nixncom@gmail.com>
2024-02-22 14:41:05 +0300

    upd

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import json
 
 from fastapi import FastAPI, responses
+from fastapi.middleware.cors import CORSMiddleware
 
 import service
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
