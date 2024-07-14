2b20889b659f2556635025519dd79602543f6791
Mikhaylov Nikita <nixncom@gmail.com>
2024-07-14 14:15:52 +0300

    upd

--- parser.py
+++ parser.py
@@ -81,12 +81,12 @@ class Parser():
         LOGGER = logging.getLogger(__name__ + ".Parser--single_threaded_parser")
         operators = []
         for page in range(self.total_pages_ok):
-            LOGGER.debug(f'Go {page+1} of {self.total_pages_ok} ok page...')
+            LOGGER.info(f'Go {page+1} of {self.total_pages_ok} ok page...')
             url = f'{self._base_url}/tables/oto/?pageNumber={page+1}'
             operators.extend(self.parse_page(url,proxy))
 
         for page in range(self.total_pages_bad):
-            LOGGER.debug(f'Go {page+1} of {self.total_pages_bad} bad page...')
+            LOGGER.info(f'Go {page+1} of {self.total_pages_bad} bad page...')
             url = f'{self._base_url}/search/oto/{page+1}?otoId=&shortName=&address=&fio=&showCanceled=true'
             operators.extend(self.parse_page(url, proxy))
 
